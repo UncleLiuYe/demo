7f5acd14eebf8b789e40c7e770325cd40ab1939b
LiuYe <215159994@qq.com>
2024-06-20 21:27:50 +0800

    update

--- src/main/java/site/liuye/servlet/MainServlet.java
+++ src/main/java/site/liuye/servlet/MainServlet.java
@@ -15,11 +15,13 @@ public class MainServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
 	}
 
 }
