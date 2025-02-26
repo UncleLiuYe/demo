b8211a8117406e3d4b025d5292036242c6544b17
LiuYe <www.215159994@qq.com>
2025-02-26 22:56:55 +0800

    first

---
+++ src/main/java/site/liuye/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package site.liuye.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1")
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
